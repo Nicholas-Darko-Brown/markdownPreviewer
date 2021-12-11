b28c9c23e7cc7f983b3ffc01d66621f4e7b912ed
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2021-12-11 18:55:03 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+marked.setOptions({
+   breaks: true,
+})
+
+const renderer = new marked.Renderer()
+renderer.link = function (href, title, text){
+   return `<a target="_blank" href="${href}">${text}</a>`
 }
 
-export default App;
+const initialState = `
+  # Header 1
+  ## Sub Header 2
+  ### Header 3
+  
+  [My GitHub Profile](https://www.github.com/Nicholas-Darko-Brown)
+  
+  \`<div>Hello World</div>\`
+  
+ \`\`\` 
+    let x = 5; 
+    let y = 2;
+    let z = x + y;
+  \`\`\`
+  
+  1. Twitter
+  2. LinkedIn
+  3. Facebook
+  
+  > This is a blockquote
+
+  ![Google Logo](https://www.google.com/logos/doodles/2021/seasonal-holidays-2021-6753651837109324-s.png)
+  
+  **Bolded Text**
+  `;
+
+const App = () => {
+   const [text, setText] = useState(initialState)
+
+   const handleChange = e => {
+      setText(e.target.value)
+   }
+
+   return (
+      <div className="my-4 container">
+          <h2 className="title text-center fw-bold">React Markdown Previewer</h2>
+          <div className="row my-5">
+            <div className="col-6">
+               <h2 className="headers">Editor</h2>
+               <textarea
+                  name="editor"
+                  id="editor"
+                  value={text}
+                  onChange={handleChange}
+                  className="my-4"
+               />
+            </div>
+
+            <div className="col-6">
+               <h2 className="headers">Previewer</h2>
+               <div
+                  className="preview my-4"
+                  id="preview"
+                  dangerouslySetInnerHTML={{
+                     __html: marked(text, { renderer: renderer })
+                  }}
+               />
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default App
